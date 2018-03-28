09a08c69c20af0207cd80fc6c4f914b6359ecd6b
kunal <kunalgk1999@gmail.com>
2018-03-28 09:58:49 +0530

    changes made for deployment

--- epcounter/settings.py
+++ epcounter/settings.py
@@ -108,3 +108,22 @@ STATIC_URL = '/static/'
 # login related info
 
 LOGIN_REDIRECT_URL = 'list_serial'
+
+
+
+##
+# HEROKU DEPLOYING
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+# STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
